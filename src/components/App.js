828c076b396dd27cc41f096e8944c9c8b4a8d166
Leandro Budau de Moraes <leandrobudau@hotmail.com>
2019-05-08 19:21:43 -0300

    Criando uma simples componente para teste

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
  * @format
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import PresentationPage from './pages/presentation'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,9 +19,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <PresentationPage />
       </View>
     );
   }
