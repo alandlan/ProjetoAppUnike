8ca56c9aff1b2a84b204d7ca952e4f684c0d0c76
Leandro Budau de Moraes <leandrobudau@hotmail.com>
2019-11-08 20:32:55 -0300

    Quase concluido o select categories

--- src/components/templates/categoryList/container/index.js
+++ src/components/templates/categoryList/container/index.js
@@ -41,38 +41,55 @@ function useValue(initialValue) {
   return [value, set];
 }
 
+function useIdsActive(data = []) {
+  const [ids, setIds] = useState();
+
+  const getIds = () => {
+    const idsActive = [];
+    data.forEach(d => {
+      d.Servicos.forEach(service => {
+        service.active && idsActive.push(service.Id);
+      });
+    });
+
+    return idsActive;
+  };
+
+  useEffect(() => {
+    setIds(getIds());
+  }, [data]);
+
+  return ids;
+}
+
 function useServices(initial) {
   const [value, setValue] = useState(initial);
+  const ids = useIdsActive(initial);
+
   const [{ data = [], loading }] = useFetch('categories');
 
   useEffect(() => {
-    // const idsActive = [];
-
-    // value.forEach(item => {
-    //   item.Servicos.forEach(service => {
-    //     idsActive.push(service.Id);
-    //   });
-    // });
-
-    setValue(data);
+    setValue(
+      data.map(d => {
+        d.Servicos = d.Servicos.map(service => {
+          service.active = ids.includes(service.Id);
+          return service;
+        });
+        return d;
+      })
+    );
   }, [data]);
 
   const active = (itm, val) => {
-    const activeItems = value.map(item => {
-      const services = item.Servicos.map(service => {
-        // const isId = itm.Id === service.Id;
-        const isId = itm.Id === service.Id;
-
-        if (isId) service.active = isId;
-
-        return service;
-      });
-
-      item.Servicos = services;
-      return item;
-    });
-
-    setValue(activeItems);
+    setValue(
+      value.map(d => {
+        d.Servicos = d.Servicos.map(service => {
+          if (itm.Id === service.Id) service.active = val;
+          return service;
+        });
+        return d;
+      })
+    );
   };
 
   return [value, loading, active];
